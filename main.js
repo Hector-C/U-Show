3144e13f0e6665f94192630dffea64388de9d4ca
Hector-C <40044957+Hector-C@users.noreply.github.com>
2018-07-12 18:49:21 -0700

    3rd try

--- main.js
+++ main.js
@@ -2,12 +2,11 @@
 var airtable_list_url = 'https://api.airtable.com/v0/app4SzizTALXtwnZi/Table%201?api_key=keyzo1ZJc5pLQgBEq'
 
 // This is were we get the JSON data from Airtable
-$.getJSON( airtsble_list_url, function( data ) {
-    console.log(data)
+$.getJSON( airtable_list_url, function( data ) {
     var items = [];
-    $.each( data.records, functional( key, val ) {
+    $.each( data.records, function( key, val ) {
         console.log(val.fields)
-        items.push('<h2>${val.fields{'Name']}</h2>');
+        items.push(`<h2>${val.fields['Name']}</h2>`);
     });
     $(".list-view").append(items.join(''));
 });

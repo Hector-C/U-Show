bc68aab01df345c12e0f1ad71426820be10316c7
Hector-C <40044957+Hector-C@users.noreply.github.com>
2018-07-19 18:34:08 -0700

    A try again

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 //This url is from airtable from the Authentication section
 var airtable_list_url = 'https://api.airtable.com/v0/app4SzizTALXtwnZi/Table%201?api_key=keyzo1ZJc5pLQgBEq'
 
+
+
 // This is were we get the JSON data from Airtable
 $.getJSON( airtable_list_url, function( data ) {
     var items = [];
     $.each( data.records, function( key, val ) {
         console.log(val.fields)
-        items.push(`<h2>${val.fields['Genre']}</h2>`);
-        items.push(`<h2>${val.fields['Sub-Genre']}</h2>`);
-        items.push(`<h2>${val.fields['Description']}</h2>`);
-        items.push(`<h2>${val.fields['Movies']}</h2>`);
+       
     });
     $(".list-view").append(items.join(''));
 });

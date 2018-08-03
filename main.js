1293c6b2cb592ad54ea4054cbada16a7ac834287
Hector-C <40044957+Hector-C@users.noreply.github.com>
2018-08-02 19:02:04 -0700

    q

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //This url is from airtable from the Authentication section
 var airtable_list_url = 'https://api.airtable.com/v0/app4SzizTALXtwnZi/Table%201?api_key=keyzo1ZJc5pLQgBEq'
 
-var cardTemplate = function(pictures, movieName, genre, rot, des ){
+var cardTemplate = function(Pictures, MovieName, Genre, RottenTomatoes, Description){
     return  `
     <div class="card-deck">
         <div class="card">
-          <img class="card-img-top" src="${pictures}" alt="Card image cap">
+          <img class="card-img-top" src="${Pictures}" alt="Card image cap">
           <div class="card-body">
-            <h5 class="card-title">${movieName}</h5>
-            <p class="card-text"> ${genre}</p>
-            <p class="card-text"> ${rot}</p>
-            <p class="card-text"> ${des}</p>
+            <h5 class="card-title">${MovieName}</h5>
+            <p class="card-text"> ${Genre}</p>
+            <p class="card-text"> ${RottenTomatoes}</p>
+            <p class="card-text"> ${Description}</p>
           
             </div>
         </div>
@@ -23,13 +23,13 @@ var cardTemplate = function(pictures, movieName, genre, rot, des ){
 $.getJSON( airtable_list_url, function( data ) {
     var items = [];
     $.each( data.records, function( key, val ) {
-      console.log(val.fields)
-      var movieName = val.fields['movieName'];
-      var genre = val.fields['genre'];
-      var des = val.fields['des']
-      var rot = val.fields['rot']
-      var pictures = val.fields['pictures'] ? val.fields['pictures'][0].url : null;
-      var html = cardTemplate( movieName, genre, pictures, rot, des);
+    console.log(val.fields);
+      var moviename = val.fields['MovieName'];
+      var genre = val.fields['Genre'];
+      var description = val.fields['Description']
+      var rottentomatoes = val.fields['RottenTomatoes']
+      var pictures = val.fields['Pictures'] ? val.fields['Pictures'][0].url : null;
+      var html = cardTemplate(moviename, genre, pictures, rottentomatoes, description);
       items.push(html);
 
     });

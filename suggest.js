f7f8763f6926952a64d0b130627cd3e7e43c7df4
Hector-C <40044957+Hector-C@users.noreply.github.com>
2018-08-12 15:28:04 -0700

    2nd Attempt

--- suggest.js
+++ suggest.js
@@ -4,21 +4,21 @@ $("#submit-suggestion").on('submit', function(e){
 
     var data = {};
     data.fields = {
-      'MovieName': $(this).find('#MovieName').val(),
-      'Genre(s)': $(this).find('#Genre').val(),
-      'Description': $(this).find('#Description').val(),
-      'RottenTomatoes': $(this).find('#RottenTomatoes').val(),
+      'MovieName:': $(this).find('#MovieName').val(),
+      'Genre(s):': $(this).find('#Genre').val(),
+      'Description:': $(this).find('#Description').val(),
+      'RottenTomatoes:': $(this).find('#RottenTomatoes').val(),
       'Pictures': [
         {
           'url': $(this).find('#Picture').val()
         }
       ],
     };
-    console.log(data.fields);
+    //console.log(data.fields);
     $.post(`https://api.airtable.com/v0/app4SzizTALXtwnZi/Table%201?api_key=keyzo1ZJc5pLQgBEq`,
     data, function () {
       // On Success
-      console.log(val.fields);
+      //console.log(val.fields);
       $("#submit-suggestion").html(`<h2>Thanks for your suggestion!</h2>`);
     }
   );
